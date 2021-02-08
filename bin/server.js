7b80b2888d68486123b25106eeccf14b2d61f768
Thiago Deodato <thiago-deodato@hotmail.com>
2021-02-08 16:27:19 -0300

    app server and route working

--- bin/server.js
+++ bin/server.js
@@ -1,27 +1,15 @@
-'use strict'
-
+const app = require('../src/app')
 const http = require('http');
 const debug = require('debug')('nodestr:server');
-const express = require('express');
 
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-var route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node API",
-        version: "0.0.1"
-    });
-});
-
-app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 console.log('API rodando na porta ' + port);
 
 function normalizePort(val) {
@@ -59,4 +47,12 @@ function onError(error) {
         default:
             throw error;
     }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof port === 'string' ?
+    'Pipe ' + port :
+    'Port ' + port;
+    debug('Listening on ' + bind);
 }
